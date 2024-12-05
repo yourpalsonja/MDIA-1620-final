9139e2623f779e7acbaf0d3ad82ff3889f8e86c0
Sonja Farrell <sonjafarrell@gmail.com>
2024-12-05 12:25:35 -0800

    Create snowglobe-store.js

---
+++ snowglobe-store.js
@@ -0,0 +1,11 @@
+/*
+You are the owner of a lovely snowglobe store, and today you have a big sale and customers waiting outside.
+
+1) You need to register the age of each customer in line outside, preferrably in an array.
+2) Your store has a "setting" object that keeps track of whether there are breakable items inside.
+2) If there are breakable items in the store, only customers over the age of 10 are allowed inside. Create a function that handles the following situations:
+- If there are breakable items, all your customers over the age of 10 should get a message logging out "Please come in!". Every customer under 10 should get a message logging "Please wait in line!".
+- If there are no breakable itmes, console log "Everyone, come inside!".
+3) Challenge: Add a setting for the age requirement to enter. By default, it should be 10, but you should be able to change it by invoking a function that accepts the new entry age as an argument. This also means that the age for the notification needs to be updated to match the new entry requirements.
+*/
+
